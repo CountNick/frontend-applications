38d1e05774837cb1e7bf398d9bb0db5f02de905c
CountNick <meijer.nick1@gmail.com>
2019-10-17 21:01:08 +0200

    :clown_face: Mocking some data

--- app/routes/instrumenten.js
+++ app/routes/instrumenten.js
@@ -5,16 +5,20 @@ export default Route.extend({
     model() {
         return [{
             title: "instrument 1",
-            description: "Description here"
+            description: "Description here",
+            image:"image link"
         }, {
             title: "instrument 2",
-            description: "Description here"
+            description: "Description here",
+            image:"image link"
         }, {
             title: "instrument 3",
-            description: "Description here"
+            description: "Description here",
+            image:"image link"
         }, {
             title: "instrument 4",
-            description: "Description here"
+            description: "Description here",
+            image:"image link"
         }]
     }
 
