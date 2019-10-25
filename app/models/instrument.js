f8f89882433e7b85259346eedef920f6121c75b5
CountNick <meijer.nick1@gmail.com>
2019-10-25 13:44:32 +0200

    :card_file_box: Now fetching data

--- app/models/instrument.js
+++ app/models/instrument.js
@@ -5,5 +5,6 @@ export default Model.extend({
     cho: DS.attr('uri'),
     placeName: DS.attr('string'),
     title: DS.attr('string'),
-    type: DS.attr('string')
+    type: DS.attr('string'),
+    picture: DS.attr('string')
 });
--- app/routes/instruments.js
+++ app/routes/instruments.js
@@ -1,11 +1,8 @@
 import Route from '@ember/routing/route';
 
-
-
-export default Route.extend({
-
-   model() {
-       return this.store.findRecord(`
+const url ="https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-36/sparql"
+//Note that the query is wrapped in es6 template strings to allow for easy copy pasting
+const query = `
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX dc: <http://purl.org/dc/elements/1.1/>
 PREFIX dct: <http://purl.org/dc/terms/>
@@ -25,8 +22,68 @@ SELECT ?cho ?placeName ?title ?type ?description WHERE {
   	dc:description ?description.
  
 }
-`);
-        
+`
+/*runQuery(url, query)
+
+function runQuery(url, query){
+  //Test if the endpoint is up and print result to page 
+  // (you can improve this script by making the next part of this function wait for a succesful result)
+  fetch(url)
+    .then(res => console.log("Status of API: " + res.status))
+	// Call the url with the query attached, output data
+	fetch(url+"?query="+ encodeURIComponent(query) +"&format=json")
+	.then(res => res.json())
+	.then(json => {
+  console.log(json.results.bindings)
+	})
+}*/
+
+
+export default Route.extend({
+
+   model() {
+       console.log("het werkt!")
+
+      return fetch(url+"?query="+ encodeURIComponent(query) +"&format=json")
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        let newArray = [];
+
+        let bindings =  json.results.bindings
+
+        //console.log(bindings)
+
+        for (var i=0; i < bindings.length; i++){
+          let item = bindings[i]
+          console.log(item)
+          
+          item.cho = item.cho.value
+          item.placeName = item.placeName.value
+          item.title = item.title.value
+          item.type = item.type.value
+          item.description = item.description.value
+          item.picture = "picture"
+          /*let newItemObject = {
+            item.cho: item.cho.value,
+            item.placeName: item.placeName.value,
+            item.title: item.title.value,
+            item.type: item.type.value,
+            item.description: item.description.value,
+            item.picture: item.picture.value
+          }*/
+          
+          
+          newArray.push(item);
+          //console.log(newArray);
+        }
+        console.log(bindings)
+        return bindings
+
+         //console.log(newArray);
+       
+       })
+
         /*return [{
             title: "instrument 1",
             description: "Description here",
