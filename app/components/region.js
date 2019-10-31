0f4e09ca06d9e6b4609ec66e9d93035ae06ece6e
CountNick <meijer.nick1@gmail.com>
2019-10-31 16:55:38 +0100

    :zap:Added click events so users can click through the pp

--- app/components/region.js
+++ app/components/region.js
@@ -1,5 +1,12 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
 
+    router: service(),
+        //transitions to the places-list route, source: https://api.emberjs.com/ember/release/classes/RouterService    
+        click(){
+            this.router.transitionTo('places-list')
+        }
+    
 });
