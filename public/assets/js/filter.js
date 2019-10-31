d16fde615fef1d9bbf5846f537757e8a9f9d1725
CountNick <meijer.nick1@gmail.com>
2019-10-31 17:00:21 +0100

    :lipstick:Added a js file to index.html

---
+++ public/assets/js/filter.js
@@ -0,0 +1,10 @@
+//https://github.com/cmda-tt/course-19-20/blob/master/examples/local-storage/nameStorage.js
+
+// const region = document.getElementsByClassName("region")
+
+// for (let i = 0; i < region.length; i++){
+//     console.log("regions: " + i[i])
+// }
+
+// //console.log("region", region)
+// //console.log("js werkt");
